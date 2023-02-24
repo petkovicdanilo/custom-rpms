d07d20ee8ab550f59f94e32465d184dfef0c3e75
Danilo Petkovic <petkovicdanilo97@gmail.com>
2023-02-25 00:14:02 +0100

    Update neovim

--- neovim/neovim-build.sh
+++ neovim/neovim-build.sh
@@ -3,4 +3,4 @@ cd /usr/local/src/neovim
 scl enable devtoolset-10 bash
 export CXX=/opt/rh/devtoolset-10/root/usr/bin/g++
 git checkout stable
-make CMAKE_BUILD_TYPE=Release CMAKE_INSTALL_PREFIX=/build install
+make CMAKE_BUILD_TYPE=Release CMAKE_INSTALL_PREFIX=/nvim install
